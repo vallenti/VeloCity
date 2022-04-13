361a523b7e4ed69b84d5d7fa19f729772f62f7d3
Valentin Kolev <vi_kolev@mobiltel.bg>
2022-04-13 15:28:21 +0300

    add trips start/end endpoints

--- VeloCity/Controllers/TripsController.cs
+++ VeloCity/Controllers/TripsController.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VeloCity.Data;
@@ -18,9 +20,12 @@ namespace VeloCity.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public TripsController(ApplicationDbContext context)
+        private UserManager<ApplicationUser> _userManager;
+
+        public TripsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: api/Trips
@@ -77,13 +82,45 @@ namespace VeloCity.Controllers
 
         // POST: api/Trips
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Trip>> PostTrip(Trip trip)
+        [HttpPost("start")]
+        public async Task<ActionResult<Trip>> StartTrip(int bikeId)
         {
+            var user = await _userManager.GetUserAsync(User);
+
+            var bike = this._context.Bikes.Find(bikeId);
+            var trip = new Trip(user, bike);
+            trip.Begin();
             _context.Trips.Add(trip);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetTrip", new { id = trip.Id }, trip);
+        }
+
+        [HttpPost("end/{id}")]
+        public async Task<IActionResult> EndTrip(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            trip.Finish();
+
+            _context.Entry(trip).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         // DELETE: api/Trips/5
@@ -101,6 +138,9 @@ namespace VeloCity.Controllers
 
             return NoContent();
         }
+
+
+        
 
         private bool TripExists(int id)
         {
--- VeloCity/Models/Trip.cs
+++ VeloCity/Models/Trip.cs
@@ -1,28 +1,47 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using VeloCity.Models.Enums;
 
 namespace VeloCity.Models
 {
     public class Trip
     {
-        public int Id { get; set; }
+        public Trip(ApplicationUser user, Bike bike)
+        {
+            this.User = user;
+            this.Bike = bike;
+        }
 
-        public DateTime Start { get; set; }
+        public int Id { get; private set; }
 
-        public DateTime? End { get; set; }
+        public DateTime Start { get; private set; }
+
+        public DateTime? End { get; private set; }
 
         public string UserId { get; set; }
 
-        public virtual ApplicationUser User { get; set; }
+        public virtual ApplicationUser User { get; private set; }
 
-        public int BikeId { get; set; }
+        public int BikeId { get; private set; }
 
-        public virtual Bike Bike { get; set; }
+        public virtual Bike Bike { get; private set; }
 
         [NotMapped]
         public double TotalAmount => this.TotalMinutes * this.Bike.BikeType.PricePerMinute;
 
         [NotMapped]
         public int TotalMinutes => (End.Value - Start).Minutes;
+
+        public void Begin()
+        {
+            this.Start = DateTime.Now;
+            this.Bike.Status = BikeStatus.Rented;
+        }
+
+        public void Finish()
+        {
+            this.End = DateTime.Now;
+            this.Bike.Status = BikeStatus.Available;
+        }
 
     }
 }
